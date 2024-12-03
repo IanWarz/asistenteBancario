683b50ba88412e7464654890b028ac541e903fc6
ian warz <iwarz.redbrisk@gmail.com>
2024-12-02 21:49:38 -0300

    Auto-generated commit

--- chunk-I6QRGPYR.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-J4EYZZR3.js";import"./chunk-ZUVOUA3K.js";import"./chunk-BBMELW6L.js";export{a as PantallaInicioComponent};
---
+++ chunk-WH6GN5UY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-OM2QHQEN.js";import"./chunk-ZUVOUA3K.js";import"./chunk-BBMELW6L.js";export{a as PantallaInicioComponent};
